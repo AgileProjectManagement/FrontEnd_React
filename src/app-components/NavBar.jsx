ad7c55ad1d4ea84c176c3d978b339e04bbc89726
Luis F. Garcia Cerecedo <luisfergar99@gmail.com>
2022-03-23 20:42:29 -0700

    starts to follow mockups

--- src/app-components/NavBar.jsx
+++ src/app-components/NavBar.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./NavBar.css";
+import { AppBar, Toolbar, IconButton, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function NavBar({ children }) {
-  return <nav>{children}</nav>;
+  return (
+    <AppBar position="static">
+      <Toolbar element="nav">
+        <IconButton
+          size="large"
+          edge="start"
+          aria-label="menu"
+          color="inherit"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h4" component="div">
+          LIT Offerup
+        </Typography>
+        {children}
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 NavBar.propTypes = {
--- src/app-pages/Home.jsx
+++ src/app-pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { Container, Typography } from "@mui/material";
 
 export default function HomePage() {
   return (
-    <section>
-      <h1>Welcome to Lit Offer Up!</h1>
-      <p>This is a placeholder home page.</p>
-      <p>Please move along!</p>
-    </section>
+    <Container component="main">
+      <Typography align="center" variant="h1" component="h1">
+        Welcome to Lit Offer Up!
+      </Typography>
+      <Typography align="center" variant="body1">
+        This is a placeholder home page content. Please move along!
+      </Typography>
+    </Container>
   );
 }
