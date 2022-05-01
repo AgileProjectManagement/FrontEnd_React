87c3f3b84d33c1b289ac43b94e48e613b444fed5
Luis F. Garcia Cerecedo <luisfergar99@gmail.com>
2022-05-01 13:32:02 -0700

    cleans up mock data

--- src/app-pages/ListingPage.jsx
+++ src/app-pages/ListingPage.jsx
@@ -11,32 +11,7 @@ export default function ListingPage() {
 
   useEffect(async () => {
     // fetch detailed item data by id
-    let listingData = await new Promise((resolve) => {
-      setTimeout(() => {
-        if (params.itemId === "0") {
-          resolve({
-            name: "Chair",
-            id: 0,
-            price: "$18.99",
-            img: {
-              url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFo0egDlRfHUgOGqH3BA-lbmmwvfh3uyjxog&usqp=CAU",
-              altText: "A chair",
-            },
-          });
-        } else {
-          resolve({
-            name: "Table",
-            id: 1,
-            price: "$50.50",
-            img: {
-              url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKnySpL-74DPqPBrLKETA1MPe3rV3jqoC-Jw&usqp=CAU",
-              altText: "A Table",
-            },
-          });
-        }
-      }, 300);
-    });
-    listingData = await callAPI("products", "GET");
+    const listingData = await callAPI("products", "GET");
     setData(listingData[params.itemId]);
   }, []);
 
